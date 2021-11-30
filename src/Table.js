3db1d35551364693f51d7b1732747531a935c01d
Ian Boyle <irboyle21@gmail.com>
2021-11-30 15:12:13 -0500

    adds read functionality to data being passed through components

--- src/Table.js
+++ src/Table.js
@@ -13,6 +13,7 @@ const TableHeader = () => {
 const TableBody = (props) => {
   const rows = props.characterData.map((row, index) => {
     return (
+      // You'll notice I've added a key index to each table row. You should always use keys when making lists in React, as they help identify each list item. We'll also see how this is necessary in a moment when we want to manipulate list items.
       <tr key={index}>
         <td>{row.name}</td>
         <td>{row.job}</td>
